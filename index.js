2e3a756124a1f98c6c53512cb75c185486f83625
Ayush Guragain <guragain.aayush7@gmail.com>
2024-04-03 14:26:45 +0545

    chabged to application

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
 app.get("/", (req, res) => {
-  res.send("App is Working");
+  res.send("appilation is Working");
 });
 app.listen(5000);
